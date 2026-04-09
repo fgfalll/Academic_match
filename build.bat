2777667556ed6d431103512b5c32ce3999a6650a
fgfalll <saynos2011@gmail.com>
2026-04-09 18:14:58 +0300

    chore: stop tracking build.bat (in .gitignore)

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Stopping running AcademicMatch.exe...
-taskkill /F /IM AcademicMatch.exe 2>nul
-timeout /t 2 /nobreak >nul
-echo Building AcademicMatch.exe...
-.\.venv\Scripts\pyinstaller.exe AcademicMatch.spec --clean
-echo.
-echo Done! Check dist\AcademicMatch.exe
-pause
